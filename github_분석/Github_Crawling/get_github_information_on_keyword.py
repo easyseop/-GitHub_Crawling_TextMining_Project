1a844b58789f0baf7bde41bc27fdac307096a8b9
JISEOP LEE <71008721+easyseop@users.noreply.github.com>
2022-07-11 23:35:48 +0900

    07 11 깃허브 기본 정보 크롤링
    특정 기업들을 대상으로  기본적인 정보를 가져오며 제목,키워드,본문, 등 정보를 가져오는 코드

---
+++ github_분석/Github_Crawling/get_github_information_on_keyword.py
@@ -0,0 +1,68 @@
+import time
+import requests
+import pandas as pd
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup as bs
+import urllib.request as ur
+
+
+def search_information(name,page_n):
+
+    df_dic = {}
+    dic_keys = ['title','descript','topic_keyword','fork','star']
+    
+    url = 'https://github.com/orgs/{}/repositories?page={}'.format(name,page_n)
+
+    html = ur.urlopen(url)
+    soup = bs(html.read(), "html.parser")
+
+    # 이름,별점,블로그,방문자 리뷰
+    repository_list = soup.find_all("li", "Box-row") # 제목
+    # print(len(repository_list)) # 한페이지당 30개의 repository가 존재 (full일 때)
+
+
+    for k in dic_keys:
+        df_dic[k] = []
+
+    for repo in repository_list:
+        title = repo.find('a','d-inline-block').text.strip() # 각 repository별 title 추출
+
+        description = repo.find('p','color-fg-muted mb-0 wb-break-word') # 각 repository별 drcript 추출
+        if description is not None:
+            descript = description.text.strip()
+        else:
+            descript = '[BLANK]'
+
+        topic_keyword = repo.find('div','d-inline-flex flex-wrap flex-items-center f6 my-1') #각 repository별 topic keyword 추출
+
+        if topic_keyword is not None:
+            topic_keyword_list = []
+            for tk in topic_keyword.find_all('a'):
+                topic_keyword_list.append(tk.text.strip())
+        else:
+            topic_keyword_list = []
+
+        get_fork = repo.find('a','Link--muted mr-3') #각 repository별 fork 수 추출
+
+        if get_fork is not None:
+            fork = int(get_fork.text.strip().replace(',',''))
+        else:
+            fork = 0
+
+        get_star = repo.find('a','no-wrap Link--muted mr-3') #각 repository별 star 수 추출
+
+        if get_star is not None:
+            star = int(get_star.text.strip().replace(',',''))
+        else:
+            star = 0
+
+
+        df_dic['title'].append(title)
+        df_dic['descript'].append(descript)
+        df_dic['topic_keyword'].append(topic_keyword_list)
+        df_dic['fork'].append(fork)
+        df_dic['star'].append(star)
+
+        temp_df = pd.DataFrame(df_dic)
+
+    return temp_df
---
+++ github_분석/Github_Crawling/main.py
@@ -0,0 +1,34 @@
+import time
+from get_github_information_on_keyword import search_information
+import requests
+import pandas as pd
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup as bs
+import urllib.request as ur
+
+def main(name):
+
+    df = pd.DataFrame(
+            columns=[
+                "title",
+                "descript",
+                "topic_keyword",
+                "fork",
+                "star",
+            ]
+        )
+
+    for i in range(1,3): # 한 페이지당 repo 30개씩 추출 가능 
+        temp_df = search_information(name,i)
+
+        df = pd.concat([df,temp_df],ignore_index=True)
+    return df
+
+
+if __name__ == "__main__":
+
+    start = time.time()
+    result_df = main('NVIDIA')
+    end = time.time()
+    print(result_df.shape)
+    print(end - start, " second")
